a6c87da7945b7b7d3de10895fa51d3ecbf3099be
Josh <josh.waldman@servisbot.com>
2026-01-17 11:30:10 -0500

    Update CountdownClock_v2.ino

--- CountdownClock_v2.ino
+++ CountdownClock_v2.ino
@@ -1,11 +1,4 @@
-/*  Rui Santos & Sara Santos - Random Nerd Tutorials
-    THIS EXAMPLE WAS TESTED WITH THE FOLLOWING HARDWARE:
-    1) ESP32-2432S028R 2.8 inch 240×320 also known as the Cheap Yellow Display (CYD): https://makeradvisor.com/tools/cyd-cheap-yellow-display-esp32-2432s028r/
-      SET UP INSTRUCTIONS: https://RandomNerdTutorials.com/cyd/
-    2) REGULAR ESP32 Dev Board + 2.8 inch 240x320 TFT Display: https://makeradvisor.com/tools/2-8-inch-ili9341-tft-240x320/ and https://makeradvisor.com/tools/esp32-dev-board-wi-fi-bluetooth/
-      SET UP INSTRUCTIONS: https://RandomNerdTutorials.com/esp32-tft/
-    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+/*  While this is my own project, I heavily leaned on combining a few tutorials from Rui Santos & Sara Santos - Random Nerd Tutorials
 */
 
 #include <SPI.h>
